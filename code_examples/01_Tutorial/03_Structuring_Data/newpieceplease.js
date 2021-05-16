04f6aa419a3793b1d987f8e2745c45369610b93e
Joris Gutjahr <joris.gutjahr@protonmail.com>
2021-05-16 19:31:38 +0200

    Applied new class definiton to examples.

--- code_examples/01_Tutorial/03_Structuring_Data/newpieceplease.js
+++ code_examples/01_Tutorial/03_Structuring_Data/newpieceplease.js
@@ -1,138 +1,132 @@
 class NewPiecePlease {
-    constructor (IPFS, OrbitDB) {
-        this.OrbitDB = OrbitDB
-        this.node = new IPFS({
-            preload: { enabled: false },
-            repo: './ipfs',
-            EXPERIMENTAL: { pubsub: true },
-            config: {
-        	    Bootstrap: [],
-        	    Addresses: { Swarm: [] }
-            }
-        })
+  constructor (Ipfs, OrbitDB) {
+    this.OrbitDB = OrbitDB
+    this.Ipfs = Ipfs
+  }
 
-        this.node.on('error', (e) => { throw (e) })
-        this.node.on('ready', this._init.bind(this))
+  async create() {
+    this.node = await this.Ipfs.create({
+      preload: { enabled: false },
+      repo: './ipfs',
+      EXPERIMENTAL: { pubsub: true },
+      config: {
+        Bootstrap: [],
+        Addresses: { Swarm: [] }
+      }
+    })
+
+    this._init()
+  }
+
+  async _init () {
+    const peerInfo = await this.node.id()
+    this.orbitdb = await this.OrbitDB.createInstance(this.node)
+    this.defaultOptions = { accessController: {
+      write: [this.orbitdb.identity.id]
+      }
     }
 
-    async _init () {
-        const peerInfo = await this.node.id()
-        this.orbitdb = await this.OrbitDB.createInstance(this.node)
-        this.defaultOptions = { accessController: { write: [this.orbitdb.identity.id] }}
-
-        const docStoreOptions = {
-          ...this.defaultOptions,
-          indexBy: 'hash',
-        }
-
-        this.pieces = await this.orbitdb.docstore('pieces', docStoreOptions)
-        await this.pieces.load()
-
-        this.user = await this.orbitdb.kvstore("user", this.defaultOptions)
-        await this.user.load()
-        await this.user.set('pieces', this.pieces.id)
-
-        await this.loadFixtureData({
-            "username": Math.floor(Math.random() * 1000000),
-            "pieces": this.pieces.id,
-            "nodeId": peerInfo.id
-        })
-
-        // when the OrbitDB docstore has loaded, intercept this method to
-        // carry out further operations.
-        this.onready()
+    const docStoreOptions = {
+      ...this.defaultOptions,
+      indexBy: 'hash',
     }
+    this.pieces = await this.orbitdb.docstore('pieces', docStoreOptions)
+    await this.pieces.load()
 
-    async addNewPiece(hash, instrument = "Piano") {
-        const existingPiece = this.pieces.get(hash)
+    this.user = await this.orbitdb.kvstore("user", this.defaultOptions)
+    await this.user.load()
 
-        if(existingPiece.length > 0) {
-            const cid = await this.updatePieceByHash(hash, instrument)
-            return;
-        }
+    await this.loadFixtureData({
+      "username": Math.floor(Math.random()* 100000),
+      "pieces": this.pieces.id,
+      "nodeId" : peerInfo.id
+    })
 
-        const dbName = "counter." + hash.substr(20,20)
-        const counter = await this.orbitdb.counter(dbName, this.defaultOptions)
+    this.onready()
+  }
 
-        const cid = await this.pieces.put({
-           hash: hash,
-           instrument: instrument,
-           counter: counter.id
-        })
+  async addNewPiece(hash, instrument = "Piano") {
+    const existingPiece = this.getPieceByHash(hash)
+    if(!existingPiece) {
+      const dbName = "counter." + hash.substr(20,20)
+      const counter = await this.orbitdb.counter(dbName, this.defaultOptions)
 
-	   return cid
+      const cid = await this.pieces.put({ hash, instrument,
+        counter: counter.id
+      })
+      return cid
+    } else {
+      return await this.updatePieceByHash(hash, instrument)
     }
+  }
 
-	getAllPieces() {
-		const pieces = this.pieces.get('')
-		return pieces
-	}
+  async updatePieceByHash(hash, instrument = piano) {
+    const piece = await this.getPieceByHash(hash)
+    piece.instrument = instrument
+    return await this.pieces.put(piece)
+  }
 
-	getPieceByHash(hash) {
-		const singlePiece = this.pieces.get(hash)[0]
-		return singlePiece
-	}
+  async deletePieceByHash(hash) {
+    return await this.pieces.del(hash)
+  }
 
-	getByInstrument(instrument) {
-	  	return this.pieces.query((piece) => piece.instrument === instrument)
-	}
+  getAllPieces() {
+    const pieces = this.pieces.get("")
+    return pieces
+  }
 
-	async updatePieceByHash(hash, instrument = "Piano") {
-		const piece = await this.getPieceByHash(hash)
-		piece.instrument = instrument
-		const cid = await this.pieces.put(piece)
-		return cid
-	}
+  getPieceByHash(hash) {
+    const singlePiece = this.pieces.get(hash)[0]
+    return singlePiece
+  }
 
-	async deletePieceByHash(hash) {
-		const cid = await this.pieces.del(hash)
-		return cid
-	}
+  getPieceByInstrument(instrument) {
+    return this.pieces.query(piece => piece.instrument === instrument)
+  }
 
-    async getPracticeCount(piece) {
-        const counter = await this.orbitdb.counter(piece.counter)
-        await counter.load()
-        return counter.value
+  async getPracticCount(piece) {
+    const counter = await this.orbitdb.counter(piece.counter)
+    await counter.load()
+    return counter.value
+  }
+
+  async incrementPracticeCounter(piece) {
+    const counter = await this.orbitdb.counter(piece.counter)
+    const cid = await counter.inc()
+    return cid
+  }
+
+  async deleteProfileField(key) {
+    const cid = await this.user.del(key)
+    return cid
+  }
+
+  getAllProfileFields() {
+    return this.user.all
+  }
+
+  getProfileField(key) {
+    return this.user.get(key)
+  }
+
+  async updateProfileField(key, value) {
+    const cid = await this.user.set(key, value)
+    return cid
+  }
+
+  async loadFixtureData(fixtureData) {
+    const fixtureKeys = Object.keys(fixtureData)
+    for(let i in fixtureKeys) {
+      let key = fixtureKeys[i]
+      if(!this.user.get(key)) await this.user.set(key, fixtureData[key])
     }
-
-    async incrementPracticeCounter(piece) {
-        const counter = await this.orbitdb.counter(piece.counter)
-        const cid = await counter.inc()
-        return cid
-    }
-
-    async deleteProfileField(key) {
-        const cid = await this.user.del(key)
-        return cid
-    }
-
-    getAllProfileFields() {
-        return this.user.all;
-    }
-
-    getProfileField(key) {
-        return this.user.get(key)
-    }
-
-    async updateProfileField(key, value) {
-        const cid = await this.user.set(key, value)
-        return cid
-    }
-
-    async loadFixtureData(fixtureData) {
-        const fixtureKeys = Object.keys(fixtureData)
-
-        for (let i in fixtureKeys) {
-            let key = fixtureKeys[i]
-
-            if(!this.user.get(key)) await this.user.set(key, fixtureData[key])
-        }
-    }
+  }
 }
 
 try {
     const Ipfs = require('ipfs')
     const OrbitDB = require('orbit-db')
+
     module.exports = exports = new NewPiecePlease(Ipfs, OrbitDB)
 } catch (e) {
     window.NPP = new NewPiecePlease(window.Ipfs, window.OrbitDB)
