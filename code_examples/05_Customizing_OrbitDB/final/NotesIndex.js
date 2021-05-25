94bddf2539e5ba42be4f6ba6fea740711563841b
Joris Gutjahr <info@democraticnet.de>
2021-05-25 20:19:22 +0200

    NotesIndex: added updateIndex skeleton.

--- code_examples/05_Customizing_OrbitDB/final/NotesIndex.js
+++ code_examples/05_Customizing_OrbitDB/final/NotesIndex.js
@@ -2,10 +2,54 @@
 
 class NotesIndex {
   constructor() {
-    this._index = {
-      notes: {},
-      comments: {}
+    this._index = {}
+  }
+
+  getNotes(cid) {
+    return this._index[cid].data
+  }
+
+  getComments(cid, flat = true) {
+    function flatten(children) {
+      return children.reduce((comments, comment) => {
+        comments.push(comment.data)
+        return comments.concat(flatten(comment.children))
+      }, [])
     }
+
+    if(flat) {
+      return flatten(this._index[cid].children).sort((a, b) => a.id - b.id)
+    } else {
+      return this._index[cid].children
+    }
+  }
+
+  updateIndex(oplog) {
+    oplog.values.reduce((handled, item) => {
+      if(!handled.includes(item.hash)) {
+        handled.push(item.hash)
+
+        switch (item.payload.op)) {
+          case "ADDNOTES":
+
+            break;
+          case "DELETENOTES":
+
+            break;
+          case "ADDCOMMENT":
+
+            break;
+          case "ADDCOMMENT":
+
+            break;
+          case "DELETECOMMENT":
+
+            break;
+          default:
+
+        }
+      }
+    }, [])
   }
 }
 
